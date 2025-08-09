65ee12d3a71faa997c7f0b6119f80cdf3056a7bb
MagistrTheOne <maxonyushko71@gmail.com>
2025-08-09 16:54:24 +0300

    trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,15 @@
 import { headers } from "next/headers";
+//caler (trpc)
 
 import { auth } from "@/lib/auth";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { redirect } from "next/navigation";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
+  //trpc
+  const data = await caller.hello({text:"Magistr server"});
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -13,6 +18,7 @@ const Page = async () => {
     redirect("/sign-in")
   }
 
+  return <p>{data.greeting}</p>
 
   return <HomeView/>
 }
